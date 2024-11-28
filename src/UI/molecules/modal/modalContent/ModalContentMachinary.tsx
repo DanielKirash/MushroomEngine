1d054d469a90230db23198b27387d3a8b6aac5c6
DanielKirash <daniel2004.dk24@gmail.com>
2024-11-29 00:35:48 +0100

    Refactor ModalContentMachinary: enhance edit mode UI, improve state handling, and update styles for better user experience

--- src/UI/molecules/modal/modalContent/ModalContentMachinary.tsx
+++ src/UI/molecules/modal/modalContent/ModalContentMachinary.tsx
@@ -8,17 +8,16 @@ import { FaSave, FaTimes } from "react-icons/fa";
 import { usePlants } from "../../../../contexts/PlantContext";
 
 const ModalContentMachinary = (plant: PlantType) => {
-   
+
     const { updatePlant } = usePlants();
 
     const [editedPlant, setEditedPlant] = useState({
         _id: plant?._id,
-        name: plant?.name ,
-        location: plant?.location ,
-        description: plant?.description ,
+        name: plant?.name,
+        location: plant?.location,
+        description: plant?.description,
     });
 
-    // Aggiorna lo stato quando cambiano i props
     useEffect(() => {
         if (plant) {
             setEditedPlant({
@@ -41,10 +40,7 @@ const ModalContentMachinary = (plant: PlantType) => {
         await updatePlant(editedPlant);
         plant.handleClose && plant.handleClose();
         plant.setEditMode && plant.setEditMode(false);
-
-        
-
-      };
+    };
 
     const handleCancel = () => {
         setEditedPlant({
@@ -54,74 +50,83 @@ const ModalContentMachinary = (plant: PlantType) => {
             description: plant.description,
         });
     };
-
     return (
-        plant.editMode ? 
-        (
-            <header className="modal-header">
-                <div className="modal-header-actions">
-                    <h1 className="modal-title">
-                        <span className="label">Impianto:</span> 
-                        <InputField 
-                            placeholder="Nome impianto" 
-                            id="name"
-                            value={editedPlant.name}
-                            onChange={(e) => handleChange('name', e.target.value)}
-                        />
-                    </h1>
-                    <div className="action-buttons">
-                        <button className="icon-button save" onClick={handleSave}>
-                            <FaSave />
-                        </button>
-                        <button className="icon-button cancel" onClick={handleCancel}>
-                            <FaTimes />
-                        </button>
-                    </div>
-                </div>
-                <div className="modal-info">
-                    <h2 className="info-item">
-                        <span className="labelDesc">Descrizione:</span>
-                        <InputField 
-                            placeholder="Descrizione" 
-                            id="description"
-                            value={editedPlant.description}
-                            onChange={(e) => handleChange('description', e.target.value)}
-                        />
-                    </h2>
-                </div>
-            </header>
-        )
-        : 
-        (
-        <div className="modal-content-wrapper">
-            <header className="modal-header">
-                <h1 className="modal-title">
-                    <span className="label">Impianto:</span> 
-                    <span className="value"> {plant.name}</span>
-                </h1>
-                <div className="modal-info">
-                    <h2 className="info-item">
-                        <span className="labelDesc">Descrizione:</span>
-                        <span className="value">{plant.description}</span>
-                    </h2>
-                    <h2 className="info-item">
-                        <span className="label">Stato:</span>
-                        <span className={"status-badge " + checkStatus(plant)}>{checkStatus(plant).toUpperCase()}</span>
-                    </h2>
-                </div>
-            </header>
-            <section className="machinery-section">
-                <h1 className="section-title">Macchinari</h1>
-                <div className="machinaryContainer">
-                    {plant.macchinari?.map((machinaryItem, index) => (
-                        <div key={index} className="machinary">
-                            <MachinaryCard {...machinaryItem} />
+        plant.editMode ? (
+            <div className="edit-mode-container">
+                <header className="modal-header">
+                    <div className="modal-header-actions">
+                        <div className="edit-mode-title">
+                            <span className="label">Impianto:</span>
+                            <InputField
+                                addClass="edit-mode-input"
+                                placeholder="Nome impianto"
+                                id="name"
+                                value={editedPlant.name}
+                                onChange={(e) => handleChange('name', e.target.value)}
+                            />
                         </div>
-                    ))}
+                    </div>
+                    <div className="modal-info">
+                        <div className="edit-mode-description">
+                            <span className="labelDesc">Descrizione:</span>
+                            <textarea
+                                className="edit-mode-textarea"
+                                placeholder="Descrizione"
+                                value={editedPlant.description}
+                                onChange={(e) => handleChange('description', e.target.value)}
+                            />
+                        </div>
+                        <div className="edit-mode-position">
+                            <span className="label">Posizione:</span>
+                            <InputField
+                                addClass="edit-mode-input-position"
+                                placeholder="Posizione"
+                                id="location"
+                                value={editedPlant.location}
+                                onChange={(e) => handleChange('location', e.target.value)}
+                            />
+                        </div>
+                    </div>
+                </header>
+                <div className="edit-mode-footer">
+                    <button className="icon-button cancel" onClick={handleCancel}>
+                        <FaTimes /> Annulla
+                    </button>
+                    <button className="icon-button save" onClick={handleSave}>
+                        <FaSave /> Salva
+                    </button>
                 </div>
-            </section>
-        </div>
-    ))
+            </div>
+        ) : (
+                <div className="modal-content-wrapper">
+                    <header className="modal-header">
+                        <h1 className="modal-title">
+                            <span className="label">Impianto:</span>
+                            <span className="value"> {plant.name}</span>
+                        </h1>
+                        <div className="modal-info">
+                            <h2 className="info-item">
+                                <span className="labelDesc">Descrizione:</span>
+                                <span className="value">{plant.description}</span>
+                            </h2>
+                            <h2 className="info-item">
+                                <span className="label">Stato:</span>
+                                <span className={"status-badge " + checkStatus(plant)}>{checkStatus(plant).toUpperCase()}</span>
+                            </h2>
+                        </div>
+                    </header>
+                    <section className="machinery-section">
+                        <h1 className="section-title">Macchinari</h1>
+                        <div className="machinaryContainer">
+                            {plant.macchinari?.map((machinaryItem, index) => (
+                                <div key={index} className="machinary">
+                                    <MachinaryCard {...machinaryItem} />
+                                </div>
+                            ))}
+                        </div>
+                    </section>
+                </div>
+            ))
 };
 
 export default ModalContentMachinary;
