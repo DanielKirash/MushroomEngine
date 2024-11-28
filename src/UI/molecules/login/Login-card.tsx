4f616803afc58074f71da5661157dc9d3db692d5
DanielKirash <dkirashchuk@studenti.apuliadigitalmaker.it>
2024-11-28 11:10:07 +0100

    Enhance InputField and LoginCard components: add onChange prop to InputField, implement state management for email and password in LoginCard, and update data fetching logic in Dashboard component.

--- src/UI/molecules/login/Login-card.tsx
+++ src/UI/molecules/login/Login-card.tsx
@@ -3,22 +3,41 @@ import LogoFull from '../../atoms/logo/LogoFull';
 import { FaUser } from "react-icons/fa";
 import { MdOutlinePassword } from "react-icons/md"
 import './login-card.css';
+import { useState } from 'react';
+
 
 
 const LoginCard = () => {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailInput = (e: any) => {
+        setEmail(e.target.value);
+        console.log(email);
+    }
+
+    const handlePasswordInput = (e: any) => {
+        setPassword(e.target.value);
+    }
+
+    function handleLogin(email: string, password: string): void {
+        throw new Error('Function not implemented.');
+    }
+
     return(
             <div className='login'>
                     <LogoFull className='medium-size'/>
                 <div className='inputField'>
                     <div className='iconAndInput'>
-                        <FaUser className='icon'/><InputField placeholder='Username' id='email'/>
+                        <FaUser className='icon'/><InputField placeholder='Username' id='email' onChange={handleEmailInput}/>
                     </div>
 
                     <div className='iconAndInput'>
-                        <MdOutlinePassword className='icon'/><InputField placeholder='Password' id='password'/>
+                        <MdOutlinePassword className='icon'/><InputField placeholder='Password' id='password' onChange={handlePasswordInput}/>
                     </div>
                 </div>
-                <button className='loginButton'>Login</button>
+                <button className='loginButton' onClick={() => handleLogin(email, password)}>Login</button>
             </div>
     )
         
