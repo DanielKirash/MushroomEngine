5180f01df581201992d8fd76003e72b745429c24
andrea <adipasquale@studenti.apuliadigitalmaker.it>
2024-11-29 10:00:06 +0100

    button create machinary

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Header from "../UI/organisms/header/Header";
 import Main from "../UI/organisms/main/Main";
 import '../pages/layout.css'
+import ButtonCrud from "../UI/atoms/buttons/ButtonCrud";
 
 export type DashboardProps = {
   setIsAuthenticated: (value: boolean) => void;
@@ -21,11 +22,16 @@ const Dashboard = (setIsAuthenticated: DashboardProps) => {
   useEffect(()=>{
     fetchData()
   },[])
+  
+  const handleAdd = () => {
+    console.log("ADD");
+}
 
     return (
       <div className="dashboard-layout">
         <Header {...setIsAuthenticated}/>
         <Main/>
+        <ButtonCrud funzioneBtn="aggiungi" testo="+" onClick={handleAdd}/>
       </div>
     )
   };
